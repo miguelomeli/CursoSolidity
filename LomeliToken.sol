2f34b8631b4294a77f16c43f0efeffee77a6e841
Miguel Lomeli <miguelomeli@gmail.com>
2019-08-10 09:51:28 -0500

    Update LomeliToken.sol

--- LomeliToken.sol
+++ LomeliToken.sol
@@ -6,51 +6,64 @@ contract LomeliToken {
     string public symbol = "LOM";
     uint256 public decimals = 18;
     uint256 public totalSupply = 100000000000000000000000000;
-    address owner;
+    address public owner;
+    address piter = 0x781fA46A5725a29d851d57C3C3F346233554AD86;
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
 
+
     
-    function LomeliToken() {
+    constructor() public{
         owner = msg.sender;
-        balanceOf[msg.sender] = totalSupply;
-        Transfer(0x0, owner, totalSupply);
+        uint256 tokens = totalSupply / 2;
+        balanceOf[msg.sender] = tokens;
+        balanceOf[piter] = tokens;
+        emit Transfer(0x0, owner, tokens);
+        emit Transfer(0x0, piter, tokens);
+        
     }
 
 
-    function transfer(address _to, uint256 _value) returns (bool success) {
+
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
         require(balanceOf[_to] + _value >= balanceOf[_to]);
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
-        Transfer(msg.sender, _to, _value);
+        emit Transfer(msg.sender, _to, _value);
         return true;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[_from] >= _value);
         require(balanceOf[_to] + _value >= balanceOf[_to]);
         require(allowance[_from][msg.sender] >= _value);
         balanceOf[_to] += _value;
         balanceOf[_from] -= _value;
         allowance[_from][msg.sender] -= _value;
-        Transfer(_from, _to, _value);
+        emit Transfer(_from, _to, _value);
         return true;
     }
 
-    function approve(address _spender, uint256 _value) returns (bool success) {
+    function approve(address _spender, uint256 _value) public returns (bool success) {
         require(_value == 0 || allowance[msg.sender][_spender] == 0);
         allowance[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+        emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    function burn(uint256 _value) {
+    function burn(uint256 _value) public {
         require(balanceOf[msg.sender] >= _value);
         balanceOf[msg.sender] -= _value;
         balanceOf[0x0] += _value;
-        Transfer(msg.sender, 0x0, _value);
+        emit Transfer(msg.sender, 0x0, _value);
     }
+    
+    
+    
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
